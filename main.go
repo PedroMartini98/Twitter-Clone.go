4725f90a9b69d934b5862009ba36cc69ab9385c0
PedroMartini <Pedroh.martini@yahoo.com.br>
2025-04-03 16:00:35 -0300

    added validate chirp

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +10,14 @@ import (
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+}
+
+type errorResponse struct {
+	Error string
+}
+
+type successResponse struct {
+	Valid bool
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(nextHandler http.Handler) http.Handler {
@@ -35,23 +44,68 @@ func main() {
 
 	})
 
+	mux.HandleFunc("/api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method != "POST" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		type chirp struct {
+			Body string
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		decodeData := chirp{}
+		err := decoder.Decode(&decodeData)
+		if err != nil {
+			log.Printf("Error decoding chirp: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if len(decodeData.Body) > 140 {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Printf("The chirp can only be 140 characters long")
+			return
+		}
+		data, err := json.Marshal(successResponse{Valid: true})
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error marshalling json data: %s", err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(data)
+	})
+
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 
 	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 
-	mux.HandleFunc("/api/metrics", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/admin/metrics", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
 
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Header().Set("Content-Type", "text/html")
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "Hits: %d", apiCfg.fileserverHits.Load())
+
+		htmlTemplate := `
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`
+
+		fmt.Fprintf(w, htmlTemplate, apiCfg.fileserverHits.Load())
 
 	})
 
-	mux.HandleFunc("/api/reset", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/admin/reset", func(w http.ResponseWriter, r *http.Request) {
 
 		if r.Method != "POST" {
 			w.WriteHeader(http.StatusMethodNotAllowed)
