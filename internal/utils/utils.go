2dda7cd74a9d611ad194de5a61b90c362ff90536
PedroMartini <Pedroh.martini@yahoo.com.br>
2025-04-23 09:10:55 -0300

    Transfered models and utils to its own directories

---
+++ internal/utils/utils.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func cleanProfane(chirp string) string {
+	profaneWords := map[string]bool{
+		"kerfuffle": true,
+		"sharbert":  true,
+		"fornax":    true,
+	}
+	words := strings.Split(chirp, " ")
+	for i, w := range words {
+		wordLower := strings.ToLower(w)
+		// Check if the word exactly matches a profane word (no punctuation)
+		if profaneWords[wordLower] {
+			words[i] = "****"
+		}
+	}
+	return strings.Join(words, " ")
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshaling data:%s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+
+	respondWithJSON(w, code, map[string]string{"error": msg})
+
+}
